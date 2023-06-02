46a93814bd5527c79a7e2d504e86e2203c067d45
KiefBC <kiefhay642@outlook.com>
2023-06-02 14:14:55 -0700

    Project Completed.

--- parsers.py
+++ parsers.py
@@ -1,5 +1,6 @@
 from flask_restful import reqparse, inputs
 
+# for creating database entries via POST method
 parser = reqparse.RequestParser()
 parser.add_argument('date', type=inputs.date,
                     help='The event date with the correct format is required! The correct format is YYYY-MM-DD!',
@@ -7,6 +8,7 @@ parser.add_argument('date', type=inputs.date,
 parser.add_argument('event', type=str, help='The event name is required!', required=True)
 parser.add_argument('event_id', type=int, help='The event id is required!', required=False)
 
+# for the WebCalendarEventsAll class but with time period
 parser_timep = reqparse.RequestParser()
 parser_timep.add_argument('start_time', type=inputs.date, help='The start time is required!', required=False)
 parser_timep.add_argument('end_time', type=inputs.date, help='The end time is required!', required=False)
